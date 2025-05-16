983ccd91a5eea7bf38ca57191ff80eb9a5c619a2
Maruf512 <marufsarkar512@gmail.com>
2025-05-16 14:21:58 +0600

    setup

--- backend/designer/serializers.py
+++ backend/designer/serializers.py
@@ -3,35 +3,43 @@ from designer.models import Designe, DesignGrid, Colors
 from django.contrib.auth.models import User
 
 
-class DesigneSerializer(serializers.ModelSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta:
-        model = Designe
-        fields = '__all__'
-
+        model = User
+        fields = ['username', 'email','password']
+    
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+    
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'username', 'email']
 
 
-class UserRegistrationSerializer(serializers.ModelSerializer):
+class DesigneSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
-    
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
+        model = Designe
+        fields = ['id', 'name', 'total_color_palettes', 'color_box_1', 'color_box_2', 'color_box_3', 'color_box_4', 'starting_position', 'machine_type', 'created_at']
 
 
 class DesignGridSerializer(serializers.ModelSerializer):
     class Meta:
         model = DesignGrid
-        fields = '__all__'
+        fields = ['id', 'design', 'color_box', 'total_pics']
 
 
 class ColorsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Colors
-        fields = '__all__'
+        fields = ['id', 'color']
+
+
