017f111b14360155b736f3f93982bb050c247dcd
sami999khan999 <sami999khan999@gmail.com>
2025-05-25 22:24:52 +0600

    done

--- frontend/src/components/layout/Dashboard.tsx
+++ frontend/src/components/layout/Dashboard.tsx
@@ -2,11 +2,17 @@
 
 import React from "react";
 import UserDetails from "../ui/UserDetails";
+import Navigator from "../ui/Navigator";
 
 const Dashboard = () => {
   return (
-    <div>
-      <UserDetails />
+    <div className="lg:m-9 m-5">
+      <div className="flex flex-col lg:flex-row justify-center lg:justify-between">
+        <Navigator />
+        <UserDetails />
+      </div>
+      <div></div>
+      <div></div>
     </div>
   );
 };
--- frontend/src/components/ui/UserDetails.tsx
+++ frontend/src/components/ui/UserDetails.tsx
@@ -1,9 +1,14 @@
 import useStateCustom from "@/hooks/useAuth";
+import apiClient from "@/lib/apiClient";
 import { AuthUserResponseType } from "@/types/auth";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 
 const UserDetails = () => {
   const { setData, data } = useStateCustom<AuthUserResponseType>();
+
+  const router = useRouter();
 
   useEffect(() => {
     const user = localStorage.getItem("user")
@@ -13,20 +18,50 @@ const UserDetails = () => {
     setData(user);
   }, []);
 
-  console.log(data);
+  const handleLogout = async () => {
+    const { error, status } = await apiClient("logout", {
+      method: "POST",
+    });
+
+    console.log(error, status);
+
+    if (status >= 200 && status < 300 && !error) {
+      localStorage.removeItem("user");
+      setData(null);
+      router.push("/auth/login");
+    }
+  };
 
   return (
-    <div>
-      <div className="">
-        <div className="image"></div>
-        <div className="details">
-          <h2>{data?.username}</h2>
-          <div>
-            <p>{data?.email}</p>
-            <p>id:{data?.id}</p>
+    <div className="bg-on-surface rounded-xl space-y-5 shadow-lg h-[13rem] flex flex-col justify-between">
+      <div className="flex gap-5 h-[55%] m-6">
+        <div className="image relative h-full aspect-square rounded-lg overflow-hidden shadow-md">
+          <Image
+            src={"/profile.png"}
+            objectFit="cover"
+            layout="fill"
+            alt="profile
+        "
+          />
+        </div>
+        <div className="details  rounded-md py-4 flex flex-col justify-between">
+          <div className="-space-y-1">
+            <h2 className="2xl:text-2xl text-xl capitalize text-basec font-semibold tracking-wider">
+              {data?.username}
+            </h2>
+            <p className="text-muted 2xl:text-base text-sm">id:{data?.id}</p>
           </div>
+          <p className="font-medium text-basec 2xl:text-xl text-lg tracking-wider line-clamp-1">
+            {data?.email}
+          </p>
         </div>
       </div>
+      <button
+        className="bg-secondary w-full rounded-b-xl flex items-center justify-center py-2 hover:bg-primary duration-200 text-on-surface text-xl font-semibold tracking-wide"
+        onClick={handleLogout}
+      >
+        Logout
+      </button>
     </div>
   );
 };
