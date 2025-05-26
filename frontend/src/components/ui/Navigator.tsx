e46f8a7e39facc494b4b242ec00490677b2c6790
Maruf512 <marufsarkar512@gmail.com>
2025-05-26 15:39:48 +0600

    added designer button

--- frontend/src/components/ui/Navigator.tsx
+++ frontend/src/components/ui/Navigator.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Navigator = () => {
-  return <div>Navigator</div>;
+  return (
+    <div className="bg-on-surface lg:w-[30rem] w-full h-[13rem] border border-on-surface lg:rounded-r-xl overflow-hidden flex justify-center mb-3">
+      <button className="p-2 w-full bg-primary m-4 lg:rounded-xl rounded-md hover:bg-secondary duration-200 text-on-surface text-2xl font-semibold tracking-wide">
+        Designer
+      </button>
+    </div>
+  );
 };
 
 export default Navigator;
